4af785fad140e15eac1a3f392b0faaedb56ff770
apexloaded <admin@apexloaded.com>
2024-06-27 07:08:43 +0100

    updated node version for deployment fix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import type { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
@@ -7,12 +6,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    //origin: 'https://dexa.ink',
-    origin: "http://localhost:3000"
+    origin: ['http://localhost:3000', 'https://dexapay.xyz'],
   });
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
-  //app.use(express.static(path.join(__dirname,))
-  await app.listen(4100);
+  await app.listen(4400);
 }
 bootstrap();
