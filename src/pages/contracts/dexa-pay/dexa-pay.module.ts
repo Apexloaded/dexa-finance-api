0f4e840e93c2090349d092040724cd6641eb5aaf
apexloaded <admin@apexloaded.com>
2024-06-27 11:09:15 +0100

    fixed auth module dependency issues

--- src/pages/contracts/dexa-pay/dexa-pay.module.ts
+++ src/pages/contracts/dexa-pay/dexa-pay.module.ts
@@ -2,14 +2,12 @@ import { Module, forwardRef } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DexaPayService } from './dexa-pay.service';
 import contractConfig from 'src/config/contract.config';
-import { AuthModule } from 'src/pages/auth/auth.module';
 import { PaymentModule } from 'src/pages/payment/payment.module';
 import { DexaPayListener } from './dexa-pay.listener';
 
 @Module({
   imports: [
     ConfigModule.forFeature(contractConfig),
-    AuthModule,
     forwardRef(() => PaymentModule),
   ],
   providers: [DexaPayService, DexaPayListener],
